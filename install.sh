c35d5e358d4e7702f54313c41f121224b106b133
click23 <55525839+click23@users.noreply.github.com>
2025-06-23 00:01:59 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-bash <(https://raw.githubusercontent.com/click23/hid/refs/heads/main/download.sh) "release"
+bash <(curl https://raw.githubusercontent.com/click23/hid/refs/heads/main/download.sh) "release"
